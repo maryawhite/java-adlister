0b41c3230f9a10d1ed6811c1936dbb4cfb492de6
Mary White <mary.a.white28@gmail.com>
2021-11-30 10:06:34 -0500

    completed exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,38 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    private int hitCount;
+
+    public void init(){
+        hitCount = 0;
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        hitCount++;
+        String searchQuery = req.getParameter("q");
+        PrintWriter out = res.getWriter();
+        out.println("<h1>Hello, World!</h1>");
+
+        if(searchQuery != null){
+            out.println("<p>Here are your results for " + searchQuery + "</p>");
+        }
+        if (searchQuery == "resetcounter"){
+            hitCount = 0;
+        }
+
+        String nameQuery = req.getParameter("name");
+        if(nameQuery != null) {
+            out.println("<h1>Hello, " + nameQuery + "</h1>");
+        }
+        String hits = "Total number of hits";
+        out.println(hits + " " + hitCount);
 
     }
 }
